4d4847848231e9403c328a0ef1aa31b9cf70e9aa
Dylan Lom <dylanlom@gmail.com>
2025-01-26 16:41:26 +1100

    Let dev-server serve static files

--- dev-server.py
+++ dev-server.py
@@ -7,6 +7,7 @@ import time
 import http.server
 import socketserver
 from http import HTTPStatus
+import mimetypes
 
 SOURCE_WIKI = 'index.html'
 PORT = int(sys.argv[1]) if len(sys.argv) > 1 else 8000
@@ -24,6 +25,15 @@ class Handler(http.server.SimpleHTTPRequestHandler):
 		self.end_headers()
 
 	def do_HEAD(self):
+		path = "./" + self.path
+		if self.path != '/index.html' and os.path.isfile(path):
+			self.send_response(HTTPStatus.OK)
+			type, _ = mimetypes.guess_type(path)
+			self.send_header('Content-Type', type)
+			self.send_header('Content-Length', os.path.getsize(path))
+			self.end_headers()
+			return path
+
 		with open('index.html', 'rb') as fp:
 			source_mtime = os.fstat(fp.fileno()).st_mtime
 			if self.path not in wikis or wikis[self.path][1] < source_mtime:
@@ -33,10 +43,16 @@ class Handler(http.server.SimpleHTTPRequestHandler):
 		self.send_header('Content-Type', 'text/html')
 		self.send_header('Content-Length', len(wikis[self.path][0]))
 		self.end_headers()
+		return None
 
 	def do_GET(self):
-		self.do_HEAD()
-		self.wfile.write(wikis[self.path][0])
+		local_file_path = self.do_HEAD()
+		if local_file_path is not None:
+			with open(local_file_path, "rb") as fp:
+				bytes = fp.read()
+				self.wfile.write(bytes)
+		else:
+			self.wfile.write(wikis[self.path][0])
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
 	print(f"serving on port: {PORT}")
